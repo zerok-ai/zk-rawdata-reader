e43258140e2538d2188375a5baa1a490132f90a6
Shivam Nagar <shivam.nagar@outlook.com>
2023-08-18 10:17:49 +0530

    added is_truncated column in pgSQL table as well

--- vzReader/models/pgSQLRawDataModel.go
+++ vzReader/models/pgSQLRawDataModel.go
@@ -11,5 +11,6 @@ type PgSQLRawDataModel struct {
 	TraceId     string  `json:"trace_id"`
 	SpanId      string  `json:"span_id"`
 	WorkloadIds string  `json:"workload_ids"`
+	IsTruncated bool    `json:"is_truncated"`
 	Time        uint64  `json:"time"`
 }
