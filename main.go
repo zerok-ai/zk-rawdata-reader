54f34d94c37e036701003bef1129b2c50acb8ee8
Shivam Nagar <shivam.nagar@outlook.com>
2023-11-10 16:41:49 +0530

    converted upid to string and testing.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/zerok-ai/zk-rawdata-reader/vzReader"
 	"github.com/zerok-ai/zk-rawdata-reader/vzReader/models"
@@ -21,9 +20,9 @@ func main() {
 	//	ClusterKey: "px-api-b5dd5d79-549e-4d65-bf4f-5a7f8840ced1",
 	//}
 	reader := vzReader.VzReader{
-		CloudAddr:  "px.democloud01.zerok.dev:443",
-		ClusterId:  "20ae1557-8446-4b36-a7b5-f671de74cf39",
-		ClusterKey: "px-api-b7a0b064-783f-4273-acbc-c6c6a43e3232",
+		CloudAddr:  "px.loadcloud01.getanton.com:443",
+		ClusterId:  "56d95a4d-47e6-4acb-88cd-2588df9b6176",
+		ClusterKey: "px-api-f8d4bc05-381d-469b-ab54-82b7128e58ee",
 
 		//CloudAddr:  "px.devcloud01.getanton.com:443",
 		//ClusterId:  "6c213fb3-a773-4d8b-b0ad-d6e1b720da6f",
@@ -35,17 +34,19 @@ func main() {
 		return
 	}
 
-	startTime := "-1h"
-	traceIds := []string{"1f488b4347162a86fb205b62f1e37acf"}
+	//startTime := "-1h"
+	//traceIds := []string{"1f488b4347162a86fb205b62f1e37acf"}
 
 	//data, _ := reader.GetPgSQLRawData(traceIds, startTime)
 	//data, _ := reader.GetMySQLRawData(traceIds, startTime)
-	data, _ := reader.GetHTTPRawData(traceIds, startTime)
+	//data, _ := reader.GetHTTPRawData(traceIds, startTime)
+	data, _ := reader.GetUPIDToServiceMap()
 
 	fmt.Printf("DataStats: %v\n", data.ResultStats)
 	fmt.Printf("DataResults: \n---\n")
 	for _, result := range data.Results {
-		obj, _ := json.Marshal(result)
-		fmt.Printf("Trace ID: %s\nTrace Data: %s\n---\n", result.TraceId, obj)
+		//obj, _ := json.Marshal(result)
+		//fmt.Printf("Trace ID: %s\nTrace Data: %s\n---\n", result.TraceId, obj)
+		fmt.Printf("UPID: %s\nService: %s\n---\n", result.UPID, result.Service)
 	}
 }
